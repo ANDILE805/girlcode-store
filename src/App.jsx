96b08b69774b42cc524f2a9df17311b53196caf6
ANDILE805 <andilenzmbatha0@gmail.com>
2023-06-19 21:10:47 +0200

    starting navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import TopBar from "./components/TopBar"
+import NavBar from './components/NavBar'
+
 
 function App() {
   
   return (
     <>
       <TopBar />
-      <div className='w-7'>GirlCode Store</div>
+      <NavBar />
      </>
   )
 }
